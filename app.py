08e9389b06b6f774a69fc764c9bbe3957616c31f
monkh <monkhsaihanuuriituya@gmail.com>
2024-03-24 21:06:57 +0000

    2nd commit. With a storage txt file added

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from forms import DataCollectionForm
-from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email
+
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config['SECRET_KEY'] = '1234'
-
-class DataCollectionForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    gender = RadioField('Gender', choices=[('male', 'Male'), ('female', 'Female'), ('they/them', 'They/Them'), ('prefer-not-to-say', 'Prefer Not to Say')], validators=[DataRequired()])
-    student_number = StringField('Student Number', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    answer = RadioField('Do you think Student clubs and society is efficient?', choices=[('yes', 'Yes'), ('no', 'No'), ('idk', "I don't know")], validators=[DataRequired()])
-    grades = StringField('Grades')
-    satisfaction = SelectField('Overall Satisfaction with Academic Experience', choices=[('excellent', 'Excellent'), ('good', 'Good'), ('average', 'Average'), ('poor', 'Poor')], validators=[DataRequired()])
-    improvements = TextAreaField('Suggestions for Improvement', validators=[DataRequired()])
-
 
 @app.route('/')
 def home():
@@ -32,11 +18,25 @@ def information():
 def succeffful():
     return render_template('successful.html')
 
-# Define a route to handle form submissions
+
 @app.route('/data_collection', methods=['GET', 'POST'])
 def data_collection():
     form = DataCollectionForm()
     if form.validate_on_submit():
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        gender = request.form['gender']
+        student_number = request.form['student_number']
+        email = request.form['email']
+        answer = request.form['answer']
+        grades = request.form['grades']
+        satisfaction = request.form['satisfaction']
+        improvements = request.form['improvements']
+
+        data = f"First Name: {first_name}\nLast Name: {last_name}\nGender: {gender}\nStudent Number: {student_number}\nEmail: {email}\nAnswer: {answer}\nGrades: {grades}\nSatisfaction: {satisfaction}\nImprovements: {improvements}\n\n"
+
+        with open('form_data.txt', 'a') as file:
+            file.write(data)
         return redirect('/successful')
     return render_template('data_collection.html', form=form)
 
